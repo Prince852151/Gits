32192aa3c12cc9ed1e0aa0620fa49de6d7f10428
Prince kumar <princekumar852151@gmail.com>
2025-08-27 16:53:00 +0530

    First Commit

---
+++ first.js
@@ -0,0 +1,15 @@
+const button = "added button";
+console.log(button);
+
+const login = "Loging a page added";
+console.log(login);
+
+const footer ="Footere added in our website"
+console.log(footer);
+const Payment = "Integrayed the payment gateway";
+console.log(Payment);
+
+const upi = "Integraye the upi";
+console.log(upi);
+
+console.log("latest change");
